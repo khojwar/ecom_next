4f8d6be526493c5d7a3ced28c213eb67799141f5
khojwar <khojwartikaram@gmail.com>
2025-10-22 14:33:30 +0545

    implementing next-auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react"
 export default function Login() {
+    const { data: session } = useSession();
+
+    if (session) {
+        return (
+            <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+                Signed in as {session.user?.email} <br />
+                <button onClick={() => signOut()}>Sign out</button>
+            </div>
+        );
+    }
     return (
         <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            Login
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
         </div>
     );
 }
